9a0c94f3430fa3fcda2202414e78adf57279f30f
TokiniainaHerve <tokiniainaherve.andrianarison@gmail.com>
2021-06-11 12:26:40 +0300

    Domain Historique et profil

--- grails-app/domain/grails/HistoriquePersonnel.groovy
+++ grails-app/domain/grails/HistoriquePersonnel.groovy
@@ -1,7 +1,27 @@
 package grails
 
+import java.sql.Timestamp
+
 class HistoriquePersonnel {
 
+    String idUser
+    String idPari
+    String idMatch
+    double montant
+    String nomEquipe1
+    String nomEquipe2
+    String avatarEquipe1
+    String avatarEquipe2
+    String textePari
+    double localisationx
+    double localisationy
+    Timestamp dateMatch
     static constraints = {
+        idUser blank: false
+        idPari blank: false
+        idMatch blank: false
+        montant blank: false
+       
+
     }
 }
--- grails-app/domain/grails/Profile.groovy
+++ grails-app/domain/grails/Profile.groovy
@@ -3,7 +3,11 @@ package grails
 class Profile {
 
     String iduser
+    String nom
+    String prenom
     double solde
     static constraints = {
+        iduser blank: false, unique: true
+        solde min: 0
     }
 }
